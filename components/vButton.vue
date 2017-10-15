9f04a41d50cb6dd1f134492573ff18780c6b25b3
Ars Bar <shiro1702@gmail.com>
2017-10-15 19:12:36 +0700

    добавление компонента кнопки, с базовым набором цветов, для быстрого создания  прототипов

---
+++ components/vButton.vue
@@ -0,0 +1,81 @@
+<template lang="pug">
+
+button.btn(v-if="type === 'submit'" name='name' v-bind:class = '((typeof (o)) === "string" ) ? ( (o!=="") ? ("btn--" + o.split(" ").join(" btn--") ): "" ) : ""') {{ text }}
+	slot
+a.btn(v-else-if="type === 'link'"  v-bind:class = '((typeof (o)) === "string" ) ? ( (o!=="") ? ("btn--" + o.split(" ").join(" btn--") ): "" ) : ""') {{ text }}
+	slot
+div(v-else-if="(typeof (type))!== 'string' " v-bind:class = '((typeof (o)) === "string" ) ? ( (o!=="") ? ("btn--" + o.split(" ").join(" btn--") ): "" ) : ""') {{ text }}
+	slot
+//template(v-else)
+		//slot
+
+</template>
+
+<script>
+//import vCell from '../components/cell.vue'
+
+    export default {
+		props: [ 'o', 'type' ],
+        data () {
+            return {
+                msg : "shiro"
+            }
+        },
+        components: {
+			//vBox,
+			//vCell
+        }
+    }
+</script>
+
+<style lang="scss">
+$basecolors: (
+	green: #4CAF50,
+	blue: #008CBA,
+	red: #f44336,
+	orange: #FF5300,
+	yellow: #FFED00,
+	gray: #e7e7e7,
+	black: #555555,
+	white: #fff,
+	purple: #5A009D
+);
+
+
+@mixin btn($basecolors) {
+    .btn {
+
+		border: none;
+
+		padding: 15px 32px;
+		text-align: center;
+		text-decoration: none;
+		display: inline-block;
+		color: #000;
+		font-size: 16px;
+
+		width: 100%;
+
+		@each $name, $code in $basecolors {
+				&--color-#{$name} {
+					background-color: #{$code};
+					color: white;
+				}
+		}
+
+		@each $name, $code in $basecolors {
+				&--border-#{$name} {
+					//background-color: #{$code};
+				    //background-color: white;
+					color: #{$code};// black;
+					border: 2px solid #{$code}; /* Green */
+				}
+
+		}
+    }
+
+}
+
+@include btn($basecolors);
+
+</style>
