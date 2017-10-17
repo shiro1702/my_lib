c87733a90f0193736317dfc2089edcfd77db9a23
Ars Bar <shiro1702@gmail.com>
2017-10-18 00:09:40 +0700

    добавлены модификаторы тени

--- components/vButton.vue
+++ components/vButton.vue
@@ -80,6 +80,19 @@ $base-sizes: (
 			border:none;
 		}
 
+		//одификатор тени
+		&--shadow{
+			box-shadow: 0 4px 6px 0 rgba(0,0,0,0.2), 0 2px 2px 0 rgba(0,0,0,0.19);
+			&-active:active{
+			box-shadow: 0 4px 6px 0 rgba(0,0,0,0.2), 0 2px 2px 0 rgba(0,0,0,0.19);
+			}
+			&-hover:hover{
+			box-shadow: 0 6px 8px 0 rgba(0,0,0,0.2), 0 3px 3px 0 rgba(0,0,0,0.19);
+			}
+
+		}
+
+
 		//модификатор заполнения всего родительского пространства
 		&--fill {
 			width: 100%;
