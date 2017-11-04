c21f79f2012bcef196605988943497fd2f4f08fd
Ars Bar <shiro1702@gmail.com>
2017-11-04 19:21:15 +0700

    добавлено смещение для корректного отображения

--- components/vChart.vue
+++ components/vChart.vue
@@ -2,9 +2,10 @@
 div.chart(v-bind:class = '((typeof (o)) === "string" ) ? ( (o!=="") ? ("chart__" + o.split(" ").join(" chart__") ): "" ) : ""')
 	svg.chart_svg(v-bind:height='h' v-bind:width='w')
 		circle.chart_progress-bg(v-bind:cx='h/2' v-bind:cy='w/2' v-bind:r='h / 2 - strokeWidth' stroke="#e5e5e5" v-bind:stroke-width='strokeWidth' fill="none" )
-		circle.chart_progress(v-bind:cx='h/2' v-bind:cy='w/2' v-bind:r='h / 2 - strokeWidth' v-bind:stroke-width='strokeWidth' fill="none" v-bind:stroke-dashoffset='((100-p)*(h/2-strokeWidth)*2*3.14159 )/100' v-bind:stroke-dasharray='(h/2-strokeWidth)*2*3.14159')
+
+		circle(v-for='item, i in preProc' v-bind:cx='h/2' v-bind:cy='w/2' v-bind:r='h / 2 - strokeWidth' v-bind:stroke-width='strokeWidth' fill="none" v-bind:stroke='item.color' v-bind:stroke-dashoffset='(-item.pre *(h/2-strokeWidth)*2*3.14159 )/100' v-bind:stroke-dasharray='((item.pVal)*(h/2-strokeWidth)*2*3.14159 )/100+", "+(h/2-strokeWidth)*2*3.14159')
 	div.chart_slot
-		//p {{p}}%
+		//p {{chartData.0.text}}
 		slot
 
 </template>
@@ -16,7 +17,17 @@ div.chart(v-bind:class = '((typeof (o)) === "string" ) ? ( (o!=="") ? ("chart__"
 			h:Number,
 			w:Number,
 			p:Number,
-			strokeWidth:{type:Number, default:4}
+			strokeWidth:{type:Number, default:4},
+			chartData:{
+				Type:Object,
+				default: function () {
+        		return [{
+							color: "#000",
+							pVal:0,
+							text: 'данные отсутсвуют.'
+					   }]
+				}
+			}
 		},// [ 'o', 'h','w','stroke-width','p' ],//o - прочие опции,
         data () {
             return {
@@ -27,11 +38,29 @@ div.chart(v-bind:class = '((typeof (o)) === "string" ) ? ( (o!=="") ? ("chart__"
 			//vBox,
 			//vCell
         },
-		methods: {
+		methods: {//((i-1)<0)?0:(-chartData[i-1].pVal)
 
 
+		},
+		computed: {
+			preProc: function () {
+				var pro=[];
+				var s=0;
+
+				for (var item of this.chartData)
+					{
+						//alert(s+"::"+item.pVal);
+						item.pre=s;
+						pro.push(item);
+						s=s+item.pVal;
+					}
+			  // `this` указывает на экземпляр vm
+			  //this.chartData[i].pVal
+				//alert(pro);
+				return pro;
+			}
+		  }
 		}
-    }
 </script>
 
 <style lang="scss">
